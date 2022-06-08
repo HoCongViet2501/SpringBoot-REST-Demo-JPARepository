9ebf4383c1d5c21915a062999e013c923bfe82f6
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-06-08 14:50:49 +0700

    first commit

---
+++ src/main/java/com/demo/springbootrestjparepositorydemo/SpringBootRestJpaRepositoryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootrestjparepositorydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJpaRepositoryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestJpaRepositoryDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/springbootrestjparepositorydemo/SpringBootRestJpaRepositoryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootrestjparepositorydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestJpaRepositoryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
