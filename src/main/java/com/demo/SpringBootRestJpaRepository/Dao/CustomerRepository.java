9ab4012a0e53398c9b75070f8fff75098c873503
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-06-08 15:29:11 +0700

    test get list customer

---
+++ src/main/java/com/demo/SpringBootRestJpaRepository/Dao/CustomerRepository.java
@@ -0,0 +1,2 @@
+package com.demo.SpringBootRestJpaRepository.Dao;public class CustomerRepository {
+}
---
+++ src/main/java/com/demo/SpringBootRestJpaRepository/service/CustomerServiceImpl.java
@@ -0,0 +1,2 @@
+package com.demo.SpringBootRestJpaRepository.service;public class CustomerServiceImpl {
+}
